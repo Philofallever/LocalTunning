36aafec9edf4703d5b6fa7d0cac948b5442f7c90
Fallever <piscovip@163.com>
2023-04-26 01:58:46 +0800

    M  iteminfo显示
    Signed-off-by: Fallever <piscovip@163.com>

--- LocalTuning/modmain.lua
+++ LocalTuning/modmain.lua
@@ -1,11 +1,13 @@
 TUNING.PIGGYBACK_SPEED_MULT = GetModConfigData("pigpack_speed_mult")
 AddPrefabPostInit("featherhat", function(featherHat)
-    if GetModConfigData("feature_hat_sanity") then
-        featherHat.components.equippable.dapperness = TUNING.DAPPERNESS_LARGE
-    end
+    if GLOBAL.TheWorld.ismastersim then
+        if GetModConfigData("feature_hat_sanity") then
+            featherHat.components.equippable.dapperness = TUNING.DAPPERNESS_LARGE
+        end
 
-    if GetModConfigData("feature_hat_duration") then
-        featherHat.components.fueled.maxfuel = TUNING.WALRUSHAT_PERISHTIME
-        featherHat.components.fueled:InitializeFuelLevel(TUNING.WALRUSHAT_PERISHTIME)
+        if GetModConfigData("feature_hat_duration") then
+            featherHat.components.fueled.maxfuel = TUNING.WALRUSHAT_PERISHTIME
+            featherHat.components.fueled:InitializeFuelLevel(TUNING.WALRUSHAT_PERISHTIME)
+        end
     end
 end)
